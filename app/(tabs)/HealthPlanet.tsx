ce1268f606750fe9a02609134a41f892fabfe965
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2024-11-16 16:03:02 +0900

    add react-native-auth

--- app/(tabs)/HealthPlanet.tsx
+++ app/(tabs)/HealthPlanet.tsx
@@ -2,7 +2,18 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
+import { AuthConfiguration, authorize } from 'react-native-app-auth';
+
+const config:AuthConfiguration = {
+    clientId: '2548.h6HtCfzqXc.apps.healthplanet.jp',
+    redirectUrl: 'http://localhost:8081/HealthPlanet',
+    scopes: ['innerscan'],
+    serviceConfiguration: {
+        authorizationEndpoint: 'https://www.healthplanet.jp/oauth/auth',
+        tokenEndpoint: 'https://www.healthplanet.jp/oauth/token',
+    },
+};
 
 export default function HealthPlanet() {
     return <ParallaxScrollView
@@ -19,9 +30,10 @@ export default function HealthPlanet() {
             light: '#D0D0D0'
         }}
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Health Planet</ThemedText>
-      </ThemedView>
+        <ThemedView style={styles.titleContainer}>
+            <ThemedText type="title">Health Planet</ThemedText>
+        </ThemedView>
+        <Button title="Sign In" onPress={signIn} />
     </ParallaxScrollView>
 }
 
@@ -37,3 +49,13 @@ const styles = StyleSheet.create({
         gap: 8,
     },
 });
+
+const signIn = async () => {
+    try {
+        const result = await authorize(config);
+        console.log('Access Token:', result.accessToken);
+        console.log('ID Token:', result.idToken);
+    } catch (error) {
+        console.error('OAuth Error:', error);
+    }
+};
